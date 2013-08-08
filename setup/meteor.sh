37c150ca33e779b5f6fbbb9787f399a269a787e1
Greg Allen <me@jga.me>
2013-08-08 14:57:50 -0700

    remove iptables from meteor

--- setup/meteor.sh
+++ setup/meteor.sh
@@ -3,7 +3,6 @@
 if [ ! -f "/usr/local/bin/meteor" ]; then
   curl https://install.meteor.com | /bin/sh
   npm install -g meteorite
-  iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8888
 else
   echo "meteor already installed"
 fi
