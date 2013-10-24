fc3755d465f1db32cc38b1a4a47a9f7f7d4268af
Greg Allen <me@jga.me>
2013-10-24 13:44:37 -0700

    updates

--- setup/base.sh
+++ setup/base.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/usr/bin/git" ]; then
   apt-get update
-  apt-get -y install vim git htop curl
+  apt-get -y install vim git htop curl make
 else
   echo "Git, vim, htop, curl already installed"
 fi
